125465532d74a749e0a05daa59a447ce8e293a03
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-24 11:37:01 -0300

    message model

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -141,6 +141,25 @@ export default class WhatsAppController{
 					img.show();
 				}
 
+				div.on('click', () => {
+
+					this.el.activeName.innerHTML = contact.name;
+					this.el.activeStatus.innerHTML = contact.status;
+
+					if (contact.photo) {
+						let img = this.el.activePhoto;
+						img.src = contact.photo;
+						img.show();
+					}
+
+					this.el.home.hide();
+
+					this.el.main.css({
+						display: 'flex'
+					});
+
+				});
+
 				this.el.contactsMessagesList.appendChild(div);
 
 			});
